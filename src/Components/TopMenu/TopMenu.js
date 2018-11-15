2a6bf0bb6c4cc3626cf2a5b5952ad59a370aac37
avervaet <arthur.vervaet@cognitive-matchbox.com>
2018-11-15 15:42:20 +0100

    new responsive menu

--- src/Components/TopMenu/TopMenu.js
+++ src/Components/TopMenu/TopMenu.js
@@ -1,13 +1,77 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './TopMenu.css'
-import TopTitle from './TopTitle/TopTitle';
 
-const topMenu = () => {
-	return(<ul className="nav justify-content-center"> 
-			<TopTitle name="PROJECTS" link="#projects"/>
-			<TopTitle name="ABOUT ME" link="#AboutMe"/>
-			<TopTitle name="CONTACT" link="#contact"/>
-			</ul>);
+class Navbar extends Component {
+  constructor() {
+    super();
+    
+    this.state = {
+      showMenu: false,
+    }
+
+    this.showMenu = this.showMenu.bind(this);
+    this.closeMenu = this.closeMenu.bind(this);
+  }
+
+  showMenu(event) {
+    event.preventDefault();
+    
+    this.setState({ showMenu: true }, () => {
+      document.addEventListener('click', this.closeMenu);
+    });
+  }
+
+  closeMenu() {
+    this.setState({ showMenu: false }, () => {
+      document.removeEventListener('click', this.closeMenu);
+    });
+  }
+
+	render() {
+		return(
+			<div>
+				<div className="laptop_navbar">
+					<ul className="nav justify-content-center"> 
+						<li className="nav-item top-div">	
+							<a className= "nav-link top-title" href="#projects"><b>PROJECTS</b></a>
+				   		</li>
+				   		<li className="nav-item top-div">
+							<a className= "nav-link top-title" href="#AboutMe"><b>ABOUT ME</b></a>
+				   		</li>
+				   		<li className="nav-item top-div">
+							<a className= "nav-link top-title" href="#contact"><b>CONTACT</b></a>
+				   		</li>
+					</ul>
+				</div>
+				<div className="mobile_navbar">
+					<ul className="nav justify-content-center"> 
+						<div>
+						<li className="nav-item top-div">
+							<i className="fa fa-th-list fa-2x menu-icon" onClick={this.showMenu}></i>	
+							<a className= "nav-link top-title" href="#projects"><b>PROJECTS</b></a>
+				   		</li>
+				   		</div>
+				   		 {
+				          this.state.showMenu
+				            ? (
+				           <div>
+					   		<li className="nav-item top-div second-item">
+								<a className= "nav-link top-title" href="#AboutMe"><b>ABOUT ME</b></a>
+					   		</li>
+					   		<li className="nav-item top-div second-item">
+								<a className= "nav-link top-title" href="#contact"><b>CONTACT</b></a>
+					   		</li>
+				   		</div>
+				   		    )
+			            : (
+			              null
+			            )
+			        }
+					</ul>
+				</div>
+			</div>
+		);
+	}
 }
 
-export default topMenu;
+export default Navbar;
