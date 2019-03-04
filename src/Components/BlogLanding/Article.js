f7e858a30b80cbf5d8a07663370bffc7e8a681c5
avervaet <arthur.vervaet@cognitive-matchbox.com>
2019-03-04 15:44:15 +0100

    adding some articles and images to blog

--- src/Components/BlogLanding/Article.js
+++ src/Components/BlogLanding/Article.js
@@ -4,7 +4,6 @@ import { withLocalize } from "react-localize-redux";
 const article = {
   width: '300px',
   height: '300px',
-  backgroundColor: 'red',
   display: 'inline-block',
   margin: '30px',
   verticalAlign: 'top',
@@ -19,13 +18,15 @@ const articleTitleWrapper = {
   backgroundColor: "rgb(0,0,0,0.5)",
   width: '100%',
   textAlign: "center",
-  padding: "10px 0"
+  padding: "10px 0",
+  color: 'white'
 }
 
 
 const Article = (props) =>{
   return(
     <div style={article}>
+      <img className="card-img-top" src={window.location.origin + '/Ressource/img/articles/' + props.img} alt={props.img} />
       <div style={articleTitleWrapper}>
         <h6>{props.title}</h6>
       </div>
--- src/Components/BlogLanding/BlogLanding.js
+++ src/Components/BlogLanding/BlogLanding.js
@@ -20,12 +20,20 @@ class BlogLanding extends Component {
         <div style={blogContainer}>
           <h1 style={blogTitle}>IA, ethique et Tocs</h1>
           <div>
-          	<Article title="Une intelligence artificielle éthique pour un recrutement éthique"/>
-          	<Article title="Intelligence et apprentissage"/>
-          	<Article title="Que voient les robots ?"/>
-          	<Article title="Le Machine Learning causera-t'il sa propre perte ?"/>
-          	<Article title="Quel rôle joue nos données"/>
-          	<Article title="L'importance de lier éthique et recherche en IA" />
+            <a href="https://www.linkedin.com/pulse/une-intelligence-artificielle-%C3%A9thique-pour-un-marc-pesah/">
+            	<Article title="Une intelligence artificielle éthique pour un recrutement éthique"
+            			img="recrutement-ethique.png"/>
+            </a>
+          	<Article title="Intelligence et apprentissage"
+                img="intelligence-apprentissage.png" />
+          	<Article title="Que voient les robots ?"
+                img="vision-robots.png" />
+          	<Article title="Le Machine Learning causera-t'il sa propre perte ?"
+                img="machine-learning-perte.png" />
+          	<Article title="Quel rôle joue nos données"
+                img="role-donnee.png"/>
+          	<Article title="L'importance de lier éthique et recherche en IA" 
+                img="ethique-recherche-ia.png"/>
           </div>
         </div>
         );
