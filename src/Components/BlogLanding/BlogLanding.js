d95d65900309579c4a9825848850eff7ade0a8d7
Arthur Vervaet <calaca912@gmail.com>
2019-08-10 00:43:54 +0200

    add forum

--- src/Components/BlogLanding/BlogLanding.js
+++ src/Components/BlogLanding/BlogLanding.js
@@ -20,19 +20,15 @@ class BlogLanding extends Component {
         <div style={blogContainer}>
           <h1 style={blogTitle}>IA, ethique et Tocs</h1>
           <div>
-            <a href="https://www.linkedin.com/pulse/une-intelligence-artificielle-%C3%A9thique-pour-un-marc-pesah/">
-            	<Article title="Une intelligence artificielle éthique pour un recrutement éthique"
-            			img="recrutement-ethique.png"/>
-            </a>
-            <a href="/blog/article/1">
+            <a href="/blog/article/5">
             	<Article title="Intelligence et apprentissage"
                   img="intelligence-apprentissage.png" />
             </a>
-            <a href="/blog/article/1">
+            <a href="/blog/article/4">
             	<Article title="Que voient les robots ?"
                   img="vision-robots.png" />
             </a>
-            <a href="/blog/article/1">
+            <a href="/blog/article/3">
           	   <Article title="Le Machine Learning causera-t'il sa propre perte ?"
                 img="machine-learning-perte.png" />
             </a>
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import './index.css'
 import {render} from 'react-snapshot';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Blog from './Blog'
-import {Article1, Article2} from './Components/BlogLanding/articles/articles'
+import {Article1, Article2, Article3, Article4, Article5} from './Components/BlogLanding/articles/articles'
 import Main from './Main';
 import Navbar from './Components/Navbar/Navbar';
 import { LocalizeProvider } from "react-localize-redux";
@@ -19,6 +19,9 @@ render(
 				<Route exact path='/blog' component={Blog} />
 				<Route exact path='/blog/article/1' component={Article1} />
 				<Route exact path='/blog/article/2' component={Article2} />
+				<Route exact path='/blog/article/3' component={Article3} />
+				<Route exact path='/blog/article/4' component={Article4} />
+				<Route exact path='/blog/article/5' component={Article5} />
 			</div>
 		</BrowserRouter>
 	</LocalizeProvider>
